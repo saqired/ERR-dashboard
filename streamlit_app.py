b5198618275da3bab1a8a46ae078f1df41f3aa43
saqi <jeffreychong2727@gmail.com>
2025-07-18 11:23:02 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ while True:
             dy=-10,
             fontSize=12,
             fontWeight='bold',
-            color='white'
+            color='grey'
         ).encode(
             x='Defects:N',
             y='Total Number:Q',
